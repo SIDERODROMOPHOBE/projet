d53b2ed393277506ca353fdd616ebf4ab42793da
SIDERODROMOPHOBE <salibaa.gabriel@gmail.com>
2023-04-01 11:47:35 +0200

    all

--- Backend/contracts/dos.sol
+++ Backend/contracts/dos.sol
@@ -2,13 +2,34 @@ pragma solidity ^0.6.0;
 
 contract dos
 {
+    
+
+    constructor() public
+    {
+        
+    }
+
+
+
+    address moi = 0x793d2996FA121F40Be379dc0204f8C91520B0884;
+
+    uint256 Last_Id =1;
+
     struct Users 
     {
         uint256 user_id;
+        address addresse;
         uint256 age;
+        string Profile_Desc;
         //add profile specs later
     }
 
     mapping(address=>Users) public User;
-    
+
+    function New_Id() internal returns(uint256)
+    {
+        Last_Id +=1;
+        return Last_Id;
+    }
+
 }
